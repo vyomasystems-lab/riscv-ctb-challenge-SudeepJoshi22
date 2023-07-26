c32cab3e8864e209ae05d2934f21dcdf5a660d34
SudeepJoshi22 <131865603+SudeepJoshi22@users.noreply.github.com>
2023-07-26 12:11:24 +0400

    Solved challenge1_logical

---
+++ tutorial/directed/test.s
@@ -0,0 +1,14 @@
+# 1 "test.S"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 31 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 32 "<command-line>" 2
+# 1 "test.S"
+# See LICENSE for license details.
+
+#*****************************************************************************
+# add.S
+#-----------------------------------------------------------------------------
+
+# Test add instruction.
